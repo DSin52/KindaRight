f0ba754bc13c60344438f3e3be0910509e442e2d
austinlg <austinlg@vt.edu>
2014-04-18 11:15:16 -0400

    UPDATE: changed test/bg.jpg

--- views/test/func.js
+++ views/test/func.js
@@ -2,26 +2,23 @@ $(window).load(function() {
 	var index = 0;
 	$('body').click(function() {
 		switch(index) {
-			case 3:
-				$('#two').css('top', '-100%');
-				$('#three').css('top', '0%');
-				index++;
-			break;
 			case 2:
-				$('#one').css('top', '-100%');
-				$('#two').css('top', '0%');
+				$('body').animate({
+			        scrollTop: $("#three").offset().top},
+			        'slow');
 				index++;
 			break;
 			case 1:
-				$('#intro').css('top', '-100%');
-				$('#one').css('top', '0%');
+				$('body').animate({
+			        scrollTop: $("#two").offset().top},
+			        'slow');
 				index++;
 				break;
 			case 0:
-				$('#intro').css('top', '0%');
+				$('body').animate({
+			        scrollTop: $("#one").offset().top},
+			        'slow');
 				index++;
-		}	
-		
-		
+		}			
 	});
 });
