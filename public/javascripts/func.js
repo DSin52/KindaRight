36a97b8939a3ba8fda60c6ee3954009a94937b80
austinlg <austinlg@vt.edu>
2014-04-19 19:40:17 -0400

    UPDATE: few changes made to the design

--- public/javascripts/func.js
+++ public/javascripts/func.js
@@ -1,40 +1,35 @@
 $(window).load(function() {
 	var index = 0;
 	$('body').click(function() {
-		switch(index) {
-			case 3:
-				$('html,body').animate({
-        			scrollTop: $("#four").offset().top},
-        			'slow');
-				index = 0;
-			break;
-			case 2:
-				$('html,body').animate({
-        			scrollTop: $("#three").offset().top},
-        			'slow');
-				index++;
-			break;
-			case 1:
-				$('html,body').animate({
-        			scrollTop: $("#two").offset().top},
-        			'slow');
-				index++;
-				break;
-			case 0:
-				$('html,body').animate({
+
+		var pos = $(window).scrollTop();
+		console.log()
+
+		if (pos < $("#one").offset().top - 5) {
+			$('html,body').animate({
         			scrollTop: $("#one").offset().top},
         			'slow');
-				index++;
-				break;
-			default:
-				$('html,body').animate({
-        			scrollTop: $("#intro").offset().top},
-        			'slow');
-				index = 0;
-				break;
 
-		}	
+			console.log(pos);
+			console.log($("#one").offset().top);
+		}
+		else if (pos < $("#two").offset().top -5) {
+			$('html,body').animate({
+        			scrollTop: $("#two").offset().top},
+        			'slow');
+		}
+		else if (pos < $("#three").offset().top - 5) {
+			$('html,body').animate({
+        			scrollTop: $("#three").offset().top},
+        			'slow');
+		}
+		else if (pos < $("#four").offset().top - 5) {
+			$('html,body').animate({
+        			scrollTop: $("#four").offset().top},
+        			'slow');
+		}
 		
 		
 	});
+
 });
